3bff1542580bbfba5e27c19f5b88be56ec06c1d1
thedannysoto <87dsoto@gmail.com>
2020-04-30 10:04:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+
+function map(arr, fn) {
+    return arr.map(x => fn(x));
+}
+
+function reduce(arr, fn, start) {
+    if (start === undefined) {
+        return arr.reduce(fn);
+    } else {
+        return arr.reduce(fn, start);
+    }
+}
